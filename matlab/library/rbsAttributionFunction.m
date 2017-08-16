5e665d6605313e3b35a91f2d0dc8a4ab9a22bcfa
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2017-08-16 16:50:24 +0100

    rbs attribution using trace of AB similarity + multitext

--- matlab/library/rbsAttributionFunction.m
+++ matlab/library/rbsAttributionFunction.m
@@ -1,24 +1,41 @@
-function out = rbsAttributionFunction(unknown, candidate_structs)
+function count = rbsAttributionFunction(unknown_structs, candidate_structs,...
+    author_no)
 
 no_candidates = length(candidate_structs);
 if no_candidates < 2
     error('Error: mumber of candidates be must at least 2')
 end
 
-scores = zeros(no_candidates,1);
+count = 0;
 
-for i =1:no_candidates
-    cand = candidate_structs(i);
+for unknown = unknown_structs
+
+    scores = zeros(no_candidates,1);
+
+    for i = 1:no_candidates
+        cand = candidate_structs(i);
+
+        bm = buildBlockMatrix([unknown, cand]);
+        Y = rbs(bm, 0.95);
+        Y = 0.5*(Y + Y');
+        od = Y(1:70, 71:end);
+
+%         similarity = ABsimilarityBlockMean(Y);
+%         scores(i) = similarity(1, 2);
+        scores(i) = trace(od)/70;
+    %     disp(num2str(scores(i)))
+    end
+
+    [~, idx] = max(scores);
+    if idx == author_no
+        count = count + 1;
+    end
     
-    bm = buildBlockMatrix([unknown, cand]);
-    Y = rbs(bm, 0.95);
-    Y = 0.5*(Y + Y');
-    
-    similarity = ABsimilarityBlockMean(Y);
-    scores(i) = similarity(1, 2);
-    disp(num2str(scores(i)))
 end
 
-[~, out] = max(scores);
-
 end
+
+
+
+  
+    
